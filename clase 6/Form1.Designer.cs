dd9f50f8409069391ac2448d95831c56def5ceae
root <mgiraudo@ies21.edu.ar>
2024-06-03 19:16:03 -0300

    Agregar archivos de proyecto.

---
+++ clase 6/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace clase_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtidproducto = new System.Windows.Forms.TextBox();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.cmbcategoria = new System.Windows.Forms.ComboBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.btninsertar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdtodos = new System.Windows.Forms.RadioButton();
+            this.rdprecio = new System.Windows.Forms.RadioButton();
+            this.txtprecios = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btninserta = new System.Windows.Forms.Button();
+            this.chedesendente = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cheasendente = new System.Windows.Forms.CheckBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "descripcion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "categoria";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(392, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "precio";
+            // 
+            // txtidproducto
+            // 
+            this.txtidproducto.Location = new System.Drawing.Point(12, 34);
+            this.txtidproducto.Name = "txtidproducto";
+            this.txtidproducto.Size = new System.Drawing.Size(100, 20);
+            this.txtidproducto.TabIndex = 4;
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(138, 34);
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtdescripcion.TabIndex = 5;
+            // 
+            // cmbcategoria
+            // 
+            this.cmbcategoria.FormattingEnabled = true;
+            this.cmbcategoria.Location = new System.Drawing.Point(256, 35);
+            this.cmbcategoria.Name = "cmbcategoria";
+            this.cmbcategoria.Size = new System.Drawing.Size(121, 21);
+            this.cmbcategoria.TabIndex = 6;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(395, 36);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(100, 20);
+            this.txtprecio.TabIndex = 7;
+            // 
+            // btninsertar
+            // 
+            this.btninsertar.Location = new System.Drawing.Point(377, 62);
+            this.btninsertar.Name = "btninsertar";
+            this.btninsertar.Size = new System.Drawing.Size(75, 23);
+            this.btninsertar.TabIndex = 8;
+            this.btninsertar.Text = "insertar";
+            this.btninsertar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cheasendente);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.chedesendente);
+            this.groupBox1.Controls.Add(this.btninserta);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.txtprecios);
+            this.groupBox1.Controls.Add(this.rdprecio);
+            this.groupBox1.Controls.Add(this.rdtodos);
+            this.groupBox1.Location = new System.Drawing.Point(12, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(481, 298);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "listar";
+            // 
+            // rdtodos
+            // 
+            this.rdtodos.AutoSize = true;
+            this.rdtodos.Location = new System.Drawing.Point(6, 28);
+            this.rdtodos.Name = "rdtodos";
+            this.rdtodos.Size = new System.Drawing.Size(51, 17);
+            this.rdtodos.TabIndex = 10;
+            this.rdtodos.TabStop = true;
+            this.rdtodos.Text = "todos";
+            this.rdtodos.UseVisualStyleBackColor = true;
+            // 
+            // rdprecio
+            // 
+            this.rdprecio.AutoSize = true;
+            this.rdprecio.Location = new System.Drawing.Point(81, 28);
+            this.rdprecio.Name = "rdprecio";
+            this.rdprecio.Size = new System.Drawing.Size(54, 17);
+            this.rdprecio.TabIndex = 11;
+            this.rdprecio.TabStop = true;
+            this.rdprecio.Text = "precio";
+            this.rdprecio.UseVisualStyleBackColor = true;
+            // 
+            // txtprecios
+            // 
+            this.txtprecios.Location = new System.Drawing.Point(147, 25);
+            this.txtprecios.Name = "txtprecios";
+            this.txtprecios.Size = new System.Drawing.Size(69, 20);
+            this.txtprecios.TabIndex = 12;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(244, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(264, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // btninserta
+            // 
+            this.btninserta.Location = new System.Drawing.Point(401, 22);
+            this.btninserta.Name = "btninserta";
+            this.btninserta.Size = new System.Drawing.Size(75, 23);
+            this.btninserta.TabIndex = 15;
+            this.btninserta.Text = "insertar";
+            this.btninserta.UseVisualStyleBackColor = true;
+            // 
+            // chedesendente
+            // 
+            this.chedesendente.AutoSize = true;
+            this.chedesendente.Location = new System.Drawing.Point(401, 51);
+            this.chedesendente.Name = "chedesendente";
+            this.chedesendente.Size = new System.Drawing.Size(82, 17);
+            this.chedesendente.TabIndex = 16;
+            this.chedesendente.Text = "desendente";
+            this.chedesendente.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 202);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // cheasendente
+            // 
+            this.cheasendente.AutoSize = true;
+            this.cheasendente.Location = new System.Drawing.Point(401, 74);
+            this.cheasendente.Name = "cheasendente";
+            this.cheasendente.Size = new System.Drawing.Size(76, 17);
+            this.cheasendente.TabIndex = 18;
+            this.cheasendente.Text = "asendente";
+            this.cheasendente.UseVisualStyleBackColor = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "codigo";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "descripcion";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "categoria";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "precio";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 420);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btninsertar);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.cmbcategoria);
+            this.Controls.Add(this.txtdescripcion);
+            this.Controls.Add(this.txtidproducto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "productos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtidproducto;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.ComboBox cmbcategoria;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.Button btninsertar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox txtprecios;
+        private System.Windows.Forms.RadioButton rdprecio;
+        private System.Windows.Forms.RadioButton rdtodos;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox chedesendente;
+        private System.Windows.Forms.Button btninserta;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox cheasendente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
